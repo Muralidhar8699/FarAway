6fc0bfa5c5cc55df110d039e9e69ab5ca282854f
Murali dhar <muralidhar8699@email.com>
2024-03-22 19:28:33 +0530

    changes applied

--- src/components/PackingList.jsx
+++ src/components/PackingList.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
-export default function PackingList({ items, handleDelete, handleToggle,handleClear }) {
+export default function PackingList({
+  items,
+  handleDelete,
+  handleToggle,
+  handleClear,
+}) {
   const [sortBy, setSortBy] = useState("input");
   let sortedItems;
   if (sortBy === "input") sortedItems = items;
@@ -16,11 +21,43 @@ export default function PackingList({ items, handleDelete, handleToggle,handleCl
         {sortedItems?.map((e, i) => {
           return (
             <div className="list_card" key={i}>
-              <input
+              <div className="checkbox-wrapper-12">
+                <div className="cbx">
+                  <input
+                    className="cbx-12"
+                    type="checkbox"
+                    value={e.packed}
+                    onChange={() => handleToggle(e.id)}
+                  />
+                  <label for="cbx-12"></label>
+                  <svg width="15" height="14" viewbox="0 0 15 14" fill="none">
+                    <path d="M2 8.36364L6.23077 12L13 2"></path>
+                  </svg>
+                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                  <defs>
+                    <filter id="goo-12">
+                      <fegaussianblur
+                        in="SourceGraphic"
+                        stddeviation="4"
+                        result="blur"
+                      ></fegaussianblur>
+                      <fecolormatrix
+                        in="blur"
+                        mode="matrix"
+                        values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7"
+                        result="goo-12"
+                      ></fecolormatrix>
+                      <feblend in="SourceGraphic" in2="goo-12"></feblend>
+                    </filter>
+                  </defs>
+                </svg>
+              </div>
+              {/* <input
                 type="checkbox"
                 value={e.packed}
                 onChange={() => handleToggle(e.id)}
-              />
+              /> */}
               <p>{e.quant}</p>
               <p style={e.packed ? { textDecoration: "line-through" } : {}}>
                 {e.desc}
@@ -36,7 +73,9 @@ export default function PackingList({ items, handleDelete, handleToggle,handleCl
           <option value="description">Sort by description</option>
           <option value="packed">Sort by packed status</option>
         </select>
-        <button onClick={handleClear} className="form_add">Clear List</button>
+        <button onClick={handleClear} className="form_add">
+          Clear List
+        </button>
       </div>
     </section>
   );
